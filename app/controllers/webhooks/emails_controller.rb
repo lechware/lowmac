9c5cede7994bf49647355a3e5bb0e840d21fbf26
Gaurav Cheema <gcheema@tecfoundary.com>
2015-06-13 14:17:46 +1000

    Minor fix

--- app/controllers/webhooks/emails_controller.rb
+++ app/controllers/webhooks/emails_controller.rb
@@ -9,7 +9,7 @@ class Webhooks::EmailsController < BaseController
     Rails.logger.log Logger::INFO, message.subject #print the subject to the logs
     Rails.logger.log Logger::INFO, message.body.decoded #print the decoded body to the logs
 
-    Delayed::Job.enqueue( WebHook::HandlerJob.new( params )) if params.has_key?(:message)
+    Delayed::Job.enqueue( WebHook::HandlerJob.new( params )) if (params.has_key?(:plain) or params.has_key?(:html))
   
     render :text => 'success', :status => 200 # a status of 404 would reject the mail
   end
