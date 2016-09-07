ffb2c84b6c3144b09dc6774fef6c9cc91b3fdb1d
sandeep <sandeep@vayu.com.au>
2016-09-07 11:32:51 +0545

    pointing to file storage in test and dev environments

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,20 @@
-CarrierWave.configure do |config|
-  config.storage    = :aws
-  config.aws_bucket = ENV["S3_BUCKET_NAME"]
-  # config.aws_acl    = :public_read
-  config.asset_host = "#{ENV["AWS_ASSET_HOST"]}/#{ENV["S3_BUCKET_NAME"]}"
-  config.aws_authenticated_url_expiration = 60 * 60 # * 24 * 365
+if Rails.env.development? or Rails.env.test?
+  CarrierWave.configure do |config| 
+    config.storage = :file
+  end
+else
+  CarrierWave.configure do |config|
+    config.storage    = :aws
+    config.aws_bucket = ENV["S3_BUCKET_NAME"]
+    # config.aws_acl    = :public_read
+    config.asset_host = "#{ENV["AWS_ASSET_HOST"]}/#{ENV["S3_BUCKET_NAME"]}"
+    config.aws_authenticated_url_expiration = 60 * 60 # * 24 * 365
 
-  config.aws_credentials = {
-    access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
-    secret_access_key: ENV["AWS_ACCESS_KEY"],
-    region:            ENV['AWS_REGION']
-  }
-  config.root = Rails.root.join('tmp')
+    config.aws_credentials = {
+      access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
+      secret_access_key: ENV["AWS_ACCESS_KEY"],
+      region:            ENV['AWS_REGION']
+    }
+    config.root = Rails.root.join('tmp')
+  end
 end
